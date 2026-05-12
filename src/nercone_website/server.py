084c980edd259ab4ac9c95ce938e48f3130c6723
nercone-dev <nercone@nercone.dev>
2026-05-12 11:27:26 +0900

    --

--- src/nercone_website/server.py
+++ src/nercone_website/server.py
@@ -47,8 +47,9 @@ templates.env.globals["get_daily_quote"] = get_daily_quote
 async def ping(request: Request):
     return PlainTextResponse("pong!", status_code=200)
 
-@app.api_route("/echo", methods=["GET"])
 async def echo(request: Request):
+    if not request.scope.get("trusted", False):
+        return render_error_page(templates=templates, request=request, status_code=403, message="<code>/echo</code>エンドポイントはデバッグ用途のため、信頼されている一部のIP範囲からのアクセスに限定して許可されています。", joke_message="のっととらすてっど")
     return JSONResponse(request.scope["log"], status_code=200)
 
 @app.api_route("/status", methods=["GET"])
