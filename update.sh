8f672f047b76e4c44b78f2dd2368c70cd34a5b71
nercone-dev <nercone@nercone.dev>
2026-05-12 18:56:17 +0900

    --

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo /usr/bin/systemctl disable nercone-website
 sudo /usr/bin/systemctl kill nercone-website
 /usr/bin/git pull --recurse-submodules
-/usr/bin/git submodule update --remote --merge
+/usr/bin/git submodule update --remote --merge --recursive
 /root/.local/bin/uv tool uninstall nercone-website || true
 /root/.local/bin/uv tool install . --upgrade
 sudo /usr/bin/systemctl enable nercone-website
