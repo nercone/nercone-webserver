c2954fee4ed3be7087dbf9dac5d2081d5dbfbc31
nercone-dev <nercone@nercone.dev>
2026-05-29 20:54:57 +0900

    --

--- src/nercone_website/logger.py
+++ src/nercone_website/logger.py
@@ -8,10 +8,10 @@ from .constants import Files
 
 class Logger:
     @staticmethod
-    def log(*contents: str, path: Path = Files.Logs.app):
+    def log(*contents: str, end: str = "\n", path: Path = Files.Logs.app):
         with path.open("a", encoding="utf-8") as f:
             fcntl.flock(f, fcntl.LOCK_EX)
-            f.write(" ".join(contents))
+            f.write(" ".join(contents) + end)
 
     @staticmethod
     def log_access(request: Request, response: Response):
@@ -35,10 +35,10 @@ class Logger:
                 "network": {"trusted": request.scope["network"].trusted}
             }
         }
-        Logger.log(json.dumps(log) + "\n", path=Files.Logs.access)
-        Logger.log(f"[{request.scope['id'].compact_text}] STATUS {response.status_code} FROM {request.client.host}:{request.client.port} TO {str(request.url)}\n")
+        Logger.log(json.dumps(log), path=Files.Logs.access)
+        Logger.log(f"[{request.scope['id'].compact_text}] STATUS {response.status_code} FROM {request.client.host}:{request.client.port} TO {str(request.url)}")
 
     @staticmethod
     def log_error(id: str, traceback: str):
-        Logger.log(f"[{id}]\n{traceback}\n", path=Files.Logs.error)
-        Logger.log(f"[{id}] STATUS 500\n")
+        Logger.log(f"[{id}]\n{traceback}", path=Files.Logs.error)
+        Logger.log(f"[{id}] STATUS 500")
